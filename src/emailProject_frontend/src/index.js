bfe32fc5c43a1ef40a2515b0d023633f535ac7fd
Sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-10-19 23:53:35 +0300

    Initial commit.

---
+++ src/emailProject_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { emailProject_backend } from "../../declarations/emailProject_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await emailProject_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
